5c924df8c3cd43beb249b4b189db829661e825d1
GiPisano <pisanogiacomo9@gmail.com>
2024-03-18 15:34:50 +0100

    add footer style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,38 +1,50 @@
 <footer>
     <div class="container">
-    <div class="row">
-        <div class="col">
-            <h4 class="text-center">DC COMICS</h4>
-            <ul>
-                @foreach($links['dc_comics'] as $link)
-                <li>{{ $link }}</li>
-                @endforeach
-            </ul>
-            <h4 class="text-center">SHOP</h4>
-            <ul>
-                @foreach($links['shop'] as $link)
-                <li>{{ $link }}</li>
-                @endforeach
-            </ul>
-        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="d-flex">
+                    <div class="col-4">
+                        <h4>DC COMICS</h4>
+                        <ul>
+                            @foreach($links['dc_comics'] as $link)
+                            <li><a href="#">{{ $link }}</a></li>
+                            @endforeach
+                        </ul>
+                        <h4>SHOP</h4>
+                        <ul>
+                            @foreach($links['shop'] as $link)
+                            <li><a href="#">{{ $link }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
 
-        <div class="col">
-        <h4 class="text-center">DC</h4>
-            <ul>
-                @foreach($links['dc'] as $link)
-                <li><a href="#">{{ $link }}</a></li>
-                @endforeach
-            </ul>
-        </div>
+                    <div class="col-4">
+                        <h4>DC</h4>
+                        <ul>
+                            @foreach($links['dc'] as $link)
+                            <li><a href="#">{{ $link }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
 
-        <div class="col">
-        <h4 class="text-center">SITES</h4>
-            <ul>
-                @foreach($links['sites'] as $link)
-                <li>{{ $link }}</li>
-                @endforeach
-            </ul>
+                    <div class="col-4">
+                        <h4>SITES</h4>
+                        <ul>
+                            @foreach($links['sites'] as $link)
+                            <li><a href="#">{{ $link }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+
+                <p>All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved. <a href="#">Cookies Settings</a></p>
+
+            </div>
+    
+            <div class="col-6">
+                <img src="{{ url('/images/dc-logo-bg.png')}}" alt="">
+            </div>
+
         </div>
-    </div>
     </div>
 </footer>
